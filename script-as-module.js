5ce1b96181f1ca60a44d0896ceb6a827874e20e5
Cory Chung <cochung@ucsd.edu>
2024-09-12 23:05:01 -0700

    Refactor JS file
    Rearranged methods and renamed variables for readability

--- script-as-module.js
+++ script-as-module.js
@@ -1,4 +1,5 @@
 (function() { 
+
     function Book(title, author, pages, read) {
         this.author = author;
         this.title = title;
@@ -26,56 +27,54 @@
             this.bookRead = document.querySelector("#read").checked;
         },
         bindEvents() {
-            this.addBookButton.addEventListener("click", () => this.showModal());
+            this.addBookButton.addEventListener("click", () => this.showDialog());
             this.form.addEventListener("submit", (event) => this.addBook(event));
-            this.bookGrid.addEventListener("click", (event) => this.cardActions(event));
+            this.bookGrid.addEventListener("click", (event) => {
+                if (event.target.classList.contains("read-button")) {
+                    this.toggleRead(event);
+                }
+                if (event.target.classList.contains("remove-button")) {
+                    this.removeBook(event);
+                }
+            });
         },
-        
         render() {
             this.bookGrid.innerHTML = "";
             for (let book of this.library) {
-                let bookInner =
-                    `<p class="title">`+book.title+`</p>
-                     <p class="author">By: `+book.author+`</p>
-                     <p class="pages">`+book.pages+` pages</p>
-                     <button type="button" class="read-button`+((book.read)?` is-read">Read`:` is-unread">Unread`)+`</button>
-                     <button type="button" class="remove-button">Remove</button>`;
                 let bookCard = document.createElement("div")
                     bookCard.classList.add("book-card");
-                    bookCard.innerHTML = bookInner;
                     bookCard.setAttribute("data",this.library.indexOf(book));
-                this.bookGrid.appendChild(bookCard);
+                    bookCard.innerHTML = 
+                        `<p class="title">`+book.title+`</p>
+                        <p class="author">By: `+book.author+`</p>
+                        <p class="pages">`+book.pages+` pages</p>
+                        <button type="button" class="read-button`+((book.read)?` is-read">Read`:` is-unread">Unread`)+`</button>
+                        <button type="button" class="remove-button">Remove</button>`;
+                this.bookGrid.appendChild(bookCard);    
             }
-        },
-        cardActions(event) {
-            console.log("click")
-            if (event.target.classList.contains("read-button")) {
-                this.toggleRead(event);
-            }
-            if (event.target.classList.contains("remove-button")) {
-                this.removeBook(event);
-            }
-        },
-        toggleRead(event) {
-            let bookToChange = this.library[event.target.parentElement.getAttribute("data")];
-            bookToChange.read = bookToChange.read ? false : true;
-            this.render();
-        },
-        removeBook(event) {
-            this.library.splice(event.target.parentElement.getAttribute("data"),1);
-            this.render();
-        },
-        showModal() {
-            this.form.reset();
-            this.addBookDialog.showModal();
         },
         addBook(event) {
             event.preventDefault();
             this.grabDialogInput();
-            if (!this.bookName||!this.bookAuthor||!this.bookPages) {return;            }
+            if (!this.bookName||!this.bookAuthor||!this.bookPages) {return;}
             this.library.push(new Book(this.bookName, this.bookAuthor, this.bookPages, this.bookRead));
             this.addBookDialog.close();
+            this.form.reset();
             this.render();
+        },
+        removeBook(event) {
+            let index = event.target.parentElement.getAttribute("data");
+            this.library.splice(index,1);
+            this.render();
+        },
+        toggleRead(event) {
+            let index = event.target.parentElement.getAttribute("data");
+            let bookToChange = this.library[index];
+            bookToChange.read = bookToChange.read ? false : true;
+            this.render();
+        },
+        showDialog() {
+            this.addBookDialog.showModal();
         },
     }
     
